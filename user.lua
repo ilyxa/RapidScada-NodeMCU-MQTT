22dad97acd8fd96240d96fb2bb62f821e9427f7f
Ilya ilyxa Tyshchenko <ilyxa@nest.org.ru>
2018-05-27 23:06:11 +0300

    Apache2 + mod_mono config, please ignore MonoMaxMemory ;)

--- user.lua
+++ user.lua
@@ -1,9 +1,21 @@
 dofile("credentials.lua")
 wifi_signal_mode = wifi.PHYMODE_G
-time_between_sensor_readings = 5000
+time_between_sensor_readings = 60000
 heartbeat = 0
-m = mqtt.Client(mqtt_client_id, 15, mqtt_username, mqtt_password, 1)
-m:lwt("esp1/online", 1, 0, 0)
+online_status = 1 -- 1 - offline 2 - online 3 - sensor error
+temperature = 0
+humidity = 0
+voltage = 0
+rssi = 0
+mac = 0
+connected = false
+
+
+mac = wifi.sta.getmac()
+mqtt_client_id = mqtt_client_id .. mac:gsub(":","")
+
+m = mqtt.Client(mqtt_client_id, 90, mqtt_username, mqtt_password, 1)
+m:lwt((mqtt_client_id).."/online", 1, 0, 0) -- offline event
 m:on("connect", function(client) print ("connected") end)
 m:on("offline", function(client) 
     print ("offline") 
@@ -15,13 +27,7 @@ m:on("message", function(client, topic, data)
     end
   end
 end)
-temperature = 0
-humidity = 0
-voltage = 0
-rssi = 0
-mac = 0
 
-connected = false
 wifi.setmode(wifi.STATION)
 wifi.setphymode(wifi_signal_mode)
 wifi.sta.config {ssid=wifi_SSID, pwd=wifi_password}
@@ -34,17 +40,20 @@ function get_sensor_Data()
     if status == dht.OK then
         --print("Temperature: "..(temp).."."..(temp % 10).." deg C")
         --print("Humidity: "..(humi).."."..(humi % 10).."%")
+        online_status = 2
     elseif status == dht.ERROR_CHECKSUM then
         print( "DHT Checksum error." )
+        online_status = 3
     elseif status == dht.ERROR_TIMEOUT then
         print( "DHT timed out." )
+        online_status = 3
     end
 end
 
 function loop()
     if wifi.sta.status() == 5 then
         tmr.stop(0)
-        print("Connecting to MQTT...")
+--        print("Connecting to MQTT...")
         m:connect( mqtt_broker_ip , mqtt_broker_port, 0, function(client)
              get_sensor_Data()
              voltage = adc.readvdd33()
@@ -56,18 +65,18 @@ function loop()
              m:publish((mqtt_client_id).."/voltage",voltage, 0, 0, function()
              m:publish((mqtt_client_id).."/rssi",rssi, 0, 0, function()
              m:publish((mqtt_client_id).."/heartbeat",heartbeat, 0, 0, function()
-             m:publish((mqtt_client_id).."/online", 2, 0, 0, function()
-                node.dsleep(time_between_sensor_readings*1000)
---		node.restart()
+             m:publish((mqtt_client_id).."/online", online_status, 0, 0, function()
+                node.dsleep(time_between_sensor_readings*1000,1)
              end) end) end) end) end) end)
         end,
         function(client, reason)
             print("error: "..reason)
-            node.restart()
+            node.dsleep(time_between_sensor_readings*1000,1)
+            node.restart() -- no exeption handling just reboot-and-forget, in worst case it takes too much power
         end)
         
     else
---        print("Connecting...")
+        print("Connecting...")
 --        print(wifi.sta.status())
     end
 end
@@ -77,8 +86,5 @@ if adc.force_init_mode(adc.INIT_VDD33)
         node.restart()
         return
     end
-
-mac = wifi.sta.getmac()
-mqtt_client_id = mqtt_client_id .. mac:gsub(":","")
 
 tmr.alarm(0, 100, 1, function() loop() end)
