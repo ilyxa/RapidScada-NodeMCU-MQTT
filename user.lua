6d1cfcc950e1cfa40f74588a5c4428dbd4a15f7b
Ilya ilyxa Tyshchenko <ilyxa@nest.org.ru>
2018-05-31 00:40:21 +0300

    update interval ; DHT pin

--- user.lua
+++ user.lua
@@ -1,5 +1,5 @@
 dofile("credentials.lua")
-wifi_signal_mode = wifi.PHYMODE_G
+wifi_signal_mode = wifi.PHYMODE_N
 time_between_sensor_readings = 60000
 heartbeat = 0
 online_status = 1 -- 1 - offline 2 - online 3 - sensor error
@@ -9,6 +9,7 @@ voltage = 0
 rssi = 0
 mac = 0
 connected = false
+dht_pin = 5 -- esp-01 = 4 esp-12 = 5
 
 mac = wifi.sta.getmac()
 mqtt_client_id = mqtt_client_id .. mac:gsub(":","")
@@ -25,6 +26,15 @@ m:on("message", function(client, topic, data)
         heartbeat = data
     end
   end
+  if (topic == "update_interval") then
+      if data ~= nil then
+          if tonumber(data) <= 300 then -- 5 minutes limit (check in scada)
+              time_between_sensor_readings = tonumber(data) * 1000
+              --print(time_between_sensor_readings)
+              --print(tonumber(data)*1000)
+          end
+      end
+  end
 end)
 
 wifi.setmode(wifi.STATION)
@@ -35,7 +45,7 @@ if client_ip ~= "" then
     wifi.sta.setip({ip=client_ip,netmask=client_netmask,gateway=client_gateway})
 end
 function get_sensor_Data()
-    status, temp, humi, temp_dec, humi_dec = dht.read(5)
+    status, temp, humi, temp_dec, humi_dec = dht.read(dht_pin)
     if status == dht.OK then
         --print("Temperature: "..(temp).."."..(temp % 10).." deg C")
         --print("Humidity: "..(humi).."."..(humi % 10).."%")
@@ -58,15 +68,16 @@ function loop()
              voltage = adc.readvdd33()
              rssi = wifi.sta.getrssi()
 
-             m:subscribe("heartbeat", 0, function() end)
+             m:subscribe({["heartbeat"]=0,["update_interval"]=0}, 0, function() end)
              m:publish((mqtt_client_id).."/temperature",temp, 0, 0, function()
              m:publish((mqtt_client_id).."/humidity",humi, 0, 0, function()
+             m:publish((mqtt_client_id).."/update_interval",time_between_sensor_readings/1000,0,0,function()
              m:publish((mqtt_client_id).."/voltage",voltage, 0, 0, function()
              m:publish((mqtt_client_id).."/rssi",rssi, 0, 0, function()
              m:publish((mqtt_client_id).."/heartbeat",heartbeat, 0, 0, function()
              m:publish((mqtt_client_id).."/online", online_status, 0, 0, function()
                 node.dsleep(time_between_sensor_readings*1000,1)
-             end) end) end) end) end) end)
+             end) end) end) end) end) end) end)
         end,
         function(client, reason)
             print("error: "..reason)
